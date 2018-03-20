68c967641cb1788f0d6011ad78d069f393f9ded4
Endy Muhardin <endy.muhardin@gmail.com>
2018-03-20 19:01:32 +0700

    starter project

---
+++ src/main/java/id/artivisi/coba/demowar/DemoWarApplication.java
@@ -0,0 +1,12 @@
+package id.artivisi.coba.demowar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWarApplication.class, args);
+	}
+}
